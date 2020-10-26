884e2fb53c6df6852f4b11023b932b706cdd5a31
Muhammad Rizky <44420394+MHDRizky0@users.noreply.github.com>
2020-10-26 15:22:17 +0700

    move spotify model to views

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,7 @@
-import os, sys
+import os, random, string
 import spotipy
 from spotipy import oauth2
-from spotipy.oauth2 import SpotifyOAuth
 from django.shortcuts import render, redirect
-from .models import getRecentTrack, getRecommendationsTrack, playlist
 
 scope = 'user-library-read user-library-modify ' \
         'user-read-recently-played playlist-modify-private ' \
@@ -13,7 +11,9 @@ cliend_id = '080e4d9856d645c396e08ec0b1088a02'
 client_secret = '26b9fef0e6fc4b11a77618ba41e9cd20'
 # Uri = 'http://127.0.0.1:8000/login/'
 Uri = 'http://pylistify.herokuapp.com/login/'
-username = sys.argv
+
+random_string = string.digits
+username = ''.join(random.choice(random_string) for i in range (100))
 
 sp_oauth = oauth2.SpotifyOAuth(cliend_id, client_secret, Uri, scope=scope, username=username)
 
@@ -21,7 +21,11 @@ def index(request):
     token_info = sp_oauth.get_cached_token()
     if token_info:
         print("Token Found")
-        return render(request, 'index.html')
+        recentTrack = getRecentTrack()
+        context = {
+            'recent_songs': recentTrack['recent_songs']
+        }
+        return render(request, 'index.html', context)
     else:
         print("Getting Token")
         auth_url = getSPOauthURI()
@@ -31,6 +35,83 @@ def index(request):
         }
         return render(request, 'index.html', context)
 
+def getRecentTrack():
+    global artist_id, track_id, sp
+    sp = spotipy.Spotify(auth_manager=sp_oauth)
+    artist_id = []
+    track_id = []
+
+    recent_song = []
+
+    # Get Artist Id and track id
+    recent_track = sp.current_user_recently_played(limit=5)
+    for idx, item in enumerate(recent_track['items']):
+        track = item['track']
+
+        recent_song.append(track['name'] + ' - ' + track['artists'][0]['name'])
+
+        # print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+        track_feature = track['id']
+        track_id.append(track_feature)
+
+        artist = track['artists'][0]['id']
+        artist_id.append(artist)
+
+    # Get track Attribute
+    global danceability, energy, tempo, loudness
+    danceability = []
+    energy = []
+    tempo = []
+    loudness = []
+    for item_id in track_id:
+        audio_features = sp.audio_features(tracks=item_id)
+        for idx, item in enumerate(audio_features):
+            danceability.append(item['danceability'])
+            energy.append(item['energy'])
+            tempo.append(item['tempo'])
+            loudness.append(item['loudness'])
+    content = {
+        'recent_songs': recent_song
+    }
+    return content
+
+# Get recommendation track based on artists and attribute
+def getRecommendationsTrack():
+    global recommendations_track_id
+    recommendations_track_id = []
+    songs = []
+    recommendations = sp.recommendations(seed_artists=artist_id, min_danceability=min(danceability),
+                                         max_danceability=max(danceability), min_tempo=min(tempo),
+                                         max_tempo=max(tempo), min_energy=min(energy), max_energy=max(energy),
+                                         min_loudness=min(loudness))
+
+    for idx, item in enumerate(recommendations['tracks']):
+        # print(idx, item['artists'][0]['name'], " – ", item['name'])
+        songs.append(item['name'] + ' - ' + item['artists'][0]['name'])
+        track_id = item['id']
+        recommendations_track_id.append(track_id)
+
+    content = {
+        'songs' : songs
+    }
+    return content
+
+def playlist(playlist_name):
+    # Get user id
+    user_profile = sp.me()
+    user_id = user_profile['id']
+
+    #Create Playlist
+    sp.user_playlist_create(user=user_id, name=playlist_name, public=True)
+
+    #Get lates playlist id
+    playlist = sp.user_playlists(user=user_id)
+    playlist_id = playlist['items'][0]['id']
+
+    #Add recommendations track to playlist
+    sp.playlist_add_items(playlist_id=playlist_id, items=recommendations_track_id)
+
 
 def mainPage(request):
     token_info = sp_oauth.get_cached_token()
@@ -39,27 +120,25 @@ def mainPage(request):
         if request.method == 'POST':
             playlist_imput_name = request.POST.get('playlist')
             playlist(playlist_imput_name)
-            os.remove('.cache')
+            os.remove('.cache-{}'.format(username))
             return render(request, 'success_notif.html')
         else:
-            recentTrack = getRecentTrack()
+
             recommendationTrack = getRecommendationsTrack()
 
             context = {
-                'artists': recentTrack['artist'],
-                'tracks': recentTrack['track_name'],
                 'songs' : recommendationTrack['songs']
-
             }
 
             return render(request, 'result.html', context)
+    else:
+        return redirect('/')
 
 def login(request):
     url = request.build_absolute_uri()
     code = sp_oauth.parse_response_code(url)
     print("Found Spotify auth code in Request URL! Trying to get valid access token...")
     token_info = sp_oauth.get_access_token(code)
-    username = token_info['access_token']
     return redirect('/')
 
 def getSPOauthURI():
